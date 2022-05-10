2adab0add2759e289feda19e5f7dce30d413281d
Ricardo <riximuslol@gmail.com>
2022-05-10 21:55:11 +0200

    updated to python 3.10 + fetched for label

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime as date
 
 URL = "https://technolino.sv-restaurant.ch/de/menuplan/"
 
 page = requests.get(URL)
 
 # parse the content of the page
-soup = BeautifulSoup(page.content, "html.parser")
+soup = BeautifulSoup(page.content, 'html.parser')
 
 # get the content of the id
-results = soup.find(id="l-main")
+results = soup.find(id='menu-plan-tab4')
+
+today = date.today().strftime("%A")
+
+match today:
+    case 'Monday':
+        print(today)
+    case 'Tuesday':
+        print(today)
+    case _:
+        print("Today is not Monday or Tuesday")
+
+
 
 #print(results.prettify())
 
-# returns all days in the "banner"
-days = results.find("ul", class_="no-bullets is-horizontal")
-#print(days)
-span_days = days.find_all("span", class_="day")
+menu_items = results.find_all(class_='menu-item')
 
-print(span_days[0].text.strip())
+for menu_item in menu_items:
+    print(menu_item.text.strip())
 
-menus = results.find("div", id="menu-plan-tab1")
 
-print(menus.prettify())
+#vegans = menu_plan.find_all(class_='label-vegan')
+#print(vegans)
+
