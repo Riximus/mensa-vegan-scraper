969307280f1bda2cf0b6d2fa2d97f6765c2f9766
Ricardo <riximuslol@gmail.com>
2022-05-11 22:32:03 +0200

    filtered vegan label and print the contents of the menu
    also print if there is no vegan menu

--- main.py
+++ main.py
@@ -4,34 +4,58 @@ from datetime import datetime as date
 
 URL = "https://technolino.sv-restaurant.ch/de/menuplan/"
 
-page = requests.get(URL)
-
-# parse the content of the page
-soup = BeautifulSoup(page.content, 'html.parser')
 
 # get the content of the id
-results = soup.find(id='menu-plan-tab4')
 
-today = date.today().strftime("%A")
-
-match today:
-    case 'Monday':
-        print(today)
-    case 'Tuesday':
-        print(today)
-    case _:
-        print("Today is not Monday or Tuesday")
-
-
-
-#print(results.prettify())
-
-menu_items = results.find_all(class_='menu-item')
-
-for menu_item in menu_items:
-    print(menu_item.text.strip())
+# TODO check if today is a week day or only search for a menu if its weekday or only send a request on Monday/Tuesday
+def today_menu(menus):
+    # today = date.today().strftime("%A")
+    today = 'Monday'
+    match today:
+        case 'Monday':
+            for menu, desc in menus.items():
+                print(menu)
+                print(desc)
+        case 'Tuesday':
+            print(today)
+        case _:
+            print("Today is not Monday or Tuesday")
 
 
-#vegans = menu_plan.find_all(class_='label-vegan')
-#print(vegans)
+def get_soup():
+    page = requests.get(URL)
+    # parse the content of the page
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
 
+
+def find_vegan_menu(soup):
+    menus_dict = {}
+    no_vegan_str = 'The menus today are not vegan 😔'
+    # get the first tab (that's usual today)
+    results = soup.find(id='menu-plan-tab3')
+
+    menu_items = results.find_all(class_='menu-item')
+    for menu_item in menu_items:
+        vegan = menu_item.find_next(class_='item-content').find(class_='label-info label-vegan')
+        if vegan is not None:
+            menu_name = menu_item.find_next(class_='menu-title')
+            menu_desc = menu_item.find_next(class_='menu-description')
+
+            # fill dictionary with menu name and description
+            menus_dict[menu_name.text.strip()] = menu_desc.text.strip()
+
+    # if dictionary empty
+    if not menus_dict:
+        return no_vegan_str
+
+    return menus_dict
+
+
+def main():
+    menus = find_vegan_menu(get_soup())
+    today_menu(menus)
+
+
+if __name__ == '__main__':
+    main()
